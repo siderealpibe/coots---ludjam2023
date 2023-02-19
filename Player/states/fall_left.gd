6cf738c4d1001c384b761e99b99429f8cf9527c2
Saheli Patel <tzurii@localhost.localdomain>
2023-02-19 17:00:11 -0500

    fixed infinite jump bug

--- Player/states/fall_left.gd
+++ Player/states/fall_left.gd
@@ -7,19 +7,20 @@ func input(event: InputEvent) -> int:
 
 func physics_process(delta: float) -> int:
 	var move = 0
-	if Input.is_action_pressed("ui_left"):
-		move = -1
-	elif Input.is_action_pressed("ui_right"):
-		move = 1
-	
-	player.velocity.x = move * player.WALK_SPEED
-	player.velocity.y += player.GRAVITY
-	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
-
 	if player.is_on_floor():
 		if move != 0:
 			return State.WalkLeft
 		else:
 			return State.IdleLeft
+	
+	if Input.is_action_pressed("ui_left"):
+		move = -1
+	
+	player.velocity.x = move * player.WALK_SPEED
+	player.velocity.y += player.GRAVITY
+	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
+	
+	if Input.is_action_pressed("ui_right"):
+		return State.FallRight
 	
 	return State.Null
--- Player/states/idle_left.gd
+++ Player/states/idle_left.gd
@@ -8,14 +8,14 @@ func input(event: InputEvent) -> int:
 	return .input(event)
 
 func physics_process(delta: float) -> int:
-	if Input.is_action_pressed("ui_left"):
-		return State.WalkLeft
-	elif Input.is_action_pressed("ui_right"):
-		return State.WalkRight
 	player.velocity.y += player.GRAVITY
 	player.velocity.x = 0
 	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
 
 	if !player.is_on_floor():
 		return State.FallLeft
+	elif Input.is_action_pressed("ui_left"):
+		return State.WalkLeft
+	elif Input.is_action_pressed("ui_right"):
+		return State.WalkRight
 	return State.Null
