b671e934035e9e22bdbf7137394a69fcff8b6464
Saheli Patel <tzurii@localhost.localdomain>
2023-02-25 16:53:12 -0500

    added level 3

--- Cutscenes/MidSceneCutscene.gd
+++ Cutscenes/MidSceneCutscene.gd
@@ -1,6 +1,9 @@
-extends 
+extends Cutscene
+
+signal scene_over
 
 func _exit():
-	$BlackScreen.show()
+	#$BlackScreen.show()
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D,  SceneTree.STRETCH_ASPECT_EXPAND, Vector2(1024,600),1)
-	get_tree().change_scene(exit_scene_path)
+	emit_signal("scene_over")
+	queue_free()
