ba786fcf94c8dc639af4930be0e72aa862ac0d29
pibe <siderealpibe@gmail.com>
2023-02-25 19:12:46 -0300

    ending and main screen done

--- MainTitle.gd
+++ MainTitle.gd
@@ -3,6 +3,9 @@ extends Control
 func _ready():
 	OS.set_window_maximized(true)
 	
-func _process(delta):
-	if Input.is_action_pressed("mouse_left") or Input.is_action_pressed("mouse_right"):
-			get_tree().change_scene("res://Cutscenes/Abduction.tscn")
+func _on_Button_pressed():
+	get_tree().change_scene("res://Cutscenes/Abduction.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Stages/Tutorial.tscn")
